1927689f727dfa70a7be0fa4e7615099e7a055fd
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-17 15:32:05 +0530

    Added Service Layer

---
+++ src/main/java/com/example/addressbook/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.addressbook.service;
+
+import com.example.addressbook.dto.UserDTO;
+import com.example.addressbook.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User createUser(UserDTO userDTO);
+    User updateUser(Long id, UserDTO userDTO);
+    void deleteUser(Long id);
+}
