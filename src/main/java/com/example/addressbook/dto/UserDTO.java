2275308395426c2b7004bbb0fbd4ec96be73fc80
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-17 15:48:34 +0530

    Added lombok library

--- src/main/java/com/example/addressbook/dto/UserDTO.java
+++ src/main/java/com/example/addressbook/dto/UserDTO.java
@@ -1,41 +1,10 @@
 package com.example.addressbook.dto;
+import lombok.Data;
 
+@Data
 public class UserDTO {
     private String name;
     private String address;
     private int pincode;
     private boolean PermanentAddress;
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(int pincode) {
-        this.pincode = pincode;
-    }
-
-    public boolean isPermanentAddress() {
-        return PermanentAddress;
-    }
-
-    public void setPermanentAddress(boolean permanentAddress) {
-        PermanentAddress = permanentAddress;
-    }
 }
