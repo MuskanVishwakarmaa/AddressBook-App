00bf30accf936bb120b7a60a6b94f6edf55ba9ac
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-17 17:11:01 +0530

    Added validation to name field

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -3,15 +3,17 @@ package com.example.addressbook.controller;
 import com.example.addressbook.dto.UserDTO;
 import com.example.addressbook.model.User;
 import com.example.addressbook.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-public class AddressBookController{
+public class AddressBookController {
 
     @Autowired
     private UserService userService;
@@ -30,20 +32,23 @@ public class AddressBookController{
 
     // POST create user
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(userService.createUser(userDTO));
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) {
+        User createdUser = userService.createUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     // POST create multiple users
     @PostMapping("/bulk")
-    public ResponseEntity<List<User>> createUsers(@RequestBody List<UserDTO> userDTOList) {
-        return ResponseEntity.ok(userService.saveAllUsers(userDTOList));
+    public ResponseEntity<List<User>> createUsers(@Valid @RequestBody List<UserDTO> userDTOList) {
+        List<User> createdUsers = userService.saveAllUsers(userDTOList);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUsers);
     }
 
     // PUT update user by ID
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO userDTO) {
+        User updatedUser = userService.updateUser(id, userDTO);
+        return ResponseEntity.ok(updatedUser);
     }
 
     // DELETE user by ID
